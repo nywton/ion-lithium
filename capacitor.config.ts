6ff4e74cc72cbbb227ce29650f7917853d028b60
Nywton Barros <nywton.barros@gmail.com>
2025-07-28 20:56:11 -0300

    Adds initial chat

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ion-lithium',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false,
+      backgroundColor: "#ffffffff",
+    },
+  },
 };
 
 export default config;
